f3b9f24407a8d746356ed482e7189cffbdf21189
Tomiwa-31 <oladimejitomiwa31@gmail.com>
2025-04-14 21:38:53 +0100

    Save the application file and requirements

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app=Flask(__name__)
+application=Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return "hello tomiwa - this is our demo on elasticbeanstalk"
 
 if __name__ =="__main__":
-    app.run(host='0.0.0.0',port=3000)
+    application.run(host='0.0.0.0',port=3000)
